6ae391d7bc77376a5398b0edb2b88afc4710dcbb
ButaMust <butrintmustafa154@gmail.com>
2025-05-06 21:38:29 +0200

    First commit

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42bed03e52c6dd69269ebdeb1fde49e584e0f99a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a01e9a620156c433616f96fc62fd0cae23f364f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
