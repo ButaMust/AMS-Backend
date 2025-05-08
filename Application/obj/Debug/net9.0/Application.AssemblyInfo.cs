06083bc8b16709af7b46597443f6e289cc428c71
ButaMust <butrintmustafa154@gmail.com>
2025-05-08 19:46:19 +0200

    new changes

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+774c2fce26c88f436baf9b399a3a0f3d5ab04631")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3bfb1fc2ff2b294c3845c6223f8db16c0d9963b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
